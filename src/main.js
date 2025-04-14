053cdf2035642e523bb604a306f6b566a8b2f746
Aina Herilala ANDRIAMIADANTSOA <aina@eufonie.fr>
2025-04-14 18:41:32 +0300

    Commit le 14/04/2025

--- src/main.js
+++ src/main.js
@@ -1,24 +1,54 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+// import './style.css'
+// import javascriptLogo from './javascript.svg'
+// import viteLogo from '/vite.svg'
+// import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// document.querySelector('#app').innerHTML = `
+//   <div>
+//     <a href="https://vite.dev" target="_blank">
+//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
+//     </a>
+//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+//     </a>
+//     <h1>Hello Vite!</h1>
+//     <div class="card">
+//       <button id="counter" type="button"></button>
+//     </div>
+//     <p class="read-the-docs">
+//       Click on the Vite logo to learn more
+//     </p>
+//   </div>
+// `
 
-setupCounter(document.querySelector('#counter'))
+// setupCounter(document.querySelector('#counter'))
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const contactRapid = document.getElementById('contactRapide');
+  const navbar = document.getElementById('navbar');
+
+  window.addEventListener('scroll', () => {
+    const valeurY = window.scrollY;
+
+    if (valeurY > 50) {
+      contactRapid.classList.add('hidden');
+      navbar.classList.add(
+        'absolute',
+        'top-0',
+        'left-0',
+        'z-50',
+      );
+    } else {
+      contactRapid.classList.remove('hidden');
+      navbar.classList.remove(
+        'absolute',
+        'top-0',
+        'left-0',
+        'z-50',
+      );
+    }
+  })
+});
